8772358c98a5b09fd772450bcd3ad8ed69faa70b
Evgeniy <evgen19-97@ya.ru>
2019-07-25 13:56:36 +0300

    start to define post function

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os 
-from flask import Flask
+from flask import Flask, request, jsonify, Response
 app = Flask(__name__)
 
 
@@ -10,7 +10,28 @@ def hello():
 
 @app.route('/imports',methods=['POST'])
 def post():
-    pass
+    if request.method == 'POST':
+    # проверяем, что прислали файл НАДО ПРОВЕРИТЬ ЧТО ЭТО JSON
+        if 'file' not in request.files:
+            return "someting went wrong 1"
+    
+    ### MagicCode 
+    ### Проверить данные 
+    ### Положить в БД
+    ### Получить import_id
+    import_id = 0
+    ###
+
+    # Если успешно, то присылаем код 201 и json файл - Как присылать HTTP статус?
+
+    return Response(jsonify(
+        {
+            "data":
+            {
+                "import_id": import_id
+            }
+        }
+    ), status=201)
 
 @app.route('/PATCH')
 def patch():
