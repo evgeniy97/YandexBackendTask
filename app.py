e35a177cf74054da96712d4382682426b0540d8a
Evgeniy <evgen19-97@ya.ru>
2019-07-29 12:23:42 +0300

    fix int problem

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ def get(import_id):
     if  not import_id.isdigit(): return Response(status=400)
 
     limit = dbLen()
-    if import_id >= limit: Response(status=400)
+    if int(import_id) >= limit: Response(status=400)
 
     return Response(
         jsonify(
